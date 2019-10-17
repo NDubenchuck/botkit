4ff7071d5e89b7ad045afa0830cc9c66abc74cea
nickellodio <nickellodio@gmail.com>
2019-10-17 16:24:46 +0300

    ser

--- server.js
+++ server.js
@@ -1,24 +1,8 @@
-// const express = require('express');
-//
-// const app =express();
-//
-// app.set('index.html');
-//
-// app.get('/', (req, res) => {
-//   res.render('index')
-// });
-//
-// const PORT = process.env.PORT || 3000;
-//
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`)
-// });
-
 const http = require('http');
 const fs = require('fs');
 
-const index = fs.readFileSync ('./index.html', 'utf8');
-const port = 6300;
+const index = fs.readFileSync ('index.html', 'utf8');
+const port = 443;
 
 const requestHandler = (req, res) => {
   if(req.url === '/favicon.ico') {
@@ -37,14 +21,3 @@ server.listen(port, (err) => {
   }
   console.log(`Server listdening on port ${port}`);
 });
-
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-//
-// express()
-// .use(express.static(path.join(__dirname, 'public')))
-// .set('views', path.join(__dirname, 'views'))
-// .set('view engine', 'ejs')
-// .get('/', (req, res) => res.render('pages/index'))
-// .listen(PORT, () => console.log(`Listening on ${ PORT }`))
