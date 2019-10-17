466772239bb62e1ac69d5f9c660d1fe53af73d7e
nickellodio <nickellodio@gmail.com>
2019-10-17 22:02:13 +0300

    ds

--- server.js
+++ server.js
@@ -1,23 +1,89 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
+//
+// const index = fs.readFileSync ('index.html', 'utf8');
+// const port = process.env.PORT || 443;
+//
+// const requestHandler = (req, res) => {
+//   if(req.url === '/favicon.ico') {
+//     return res.end('');
+//   }
+//   console.log(req.url, req.method);
+//   res.setHeader('content-type', 'text/html; charset=utf-8');
+//   res.end(index);
+// };
+//
+// const server = http.createServer(requestHandler);
+//
+// server.listen(port, (err) => {
+//   if (err) {
+//     console.error("eroorr", err)
+//   }
+//   console.log(`Server listdening on port ${port}`);
+// });
 
-const index = fs.readFileSync ('index.html', 'utf8');
-const port = process.env.PORT || 443;
 
-const requestHandler = (req, res) => {
-  if(req.url === '/favicon.ico') {
-    return res.end('');
+'use strict';
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Creates the endpoint for our webhook
+app.post('/webhook', (req, res) => {
+  
+  let body = req.body;
+  
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+    
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      
+      // Gets the message. entry.messaging is an array, but
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+    
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
-  console.log(req.url, req.method);
-  res.setHeader('content-type', 'text/html; charset=utf-8');
-  res.end(index);
-};
+  
+});
 
-const server = http.createServer(requestHandler);
-
-server.listen(port, (err) => {
-  if (err) {
-    console.error("eroorr", err)
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+  
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+  
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+      
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
   }
-  console.log(`Server listdening on port ${port}`);
 });
