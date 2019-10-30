60b04fd2afb43d26113ef63c997f748515711c9d
nickellodio <nickellodio@gmail.com>
2019-10-30 02:19:18 +0200

    configurated eslint

--- features/facebook_features.js
+++ features/facebook_features.js
@@ -8,7 +8,7 @@ module.exports = function(controller) {
     /**
      * Detect when a message has a sticker attached
      */
-    controller.hears(async(message) => message.sticker_id, 'message', async(bot, message) => {
+    controller.hears(async (message) => message.sticker_id, 'message', async (bot, message) => {
         await bot.reply(message,'Cool sticker.');
     });
     
--- features/sample_echo.js
+++ features/sample_echo.js
@@ -5,7 +5,7 @@
 
 module.exports = function(controller) {
 
-    controller.on('message,direct_message', async(bot, message) => {
+    controller.on('message,direct_message', async (bot, message) => {
         await bot.reply(message, `I heared \'${ message.text }\', but I dont undestend, what you mean. Sorry(`);
     });
 
