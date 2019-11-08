089c96696e3e012472d6ad6d02550f84a35a94e5
nickellodio <nickellodio@gmail.com>
2019-11-08 15:30:11 +0200

    require crashed

--- bot.js
+++ bot.js
@@ -1,98 +1,19 @@
-// //  __   __  ___        ___
-// // |__) /  \  |  |__/ |  |
-// // |__) \__/  |  |  \ |  |
-//
-// // This is the main file for the facebot bot.
-//
-// // Import Botkit's core features
-// const { Botkit } = require('botkit');
-// const { BotkitCMSHelper } = require('botkit-plugin-cms');
-//
-//
-// // Import a platform-specific adapter for facebook.
-//
-// const { FacebookAdapter, FacebookEventTypeMiddleware } = require('botbuilder-adapter-facebook');
-//
-// const { MongoDbStorage } = require('botbuilder-storage-mongodb');// const mongoose = require('mongoose');
-//
-// // Load process.env values from .env file
-// require('dotenv').config();
-//
-// let storage = null;
-// if (process.env.MONGO_URI) {
-//   storage = mongoStorage = new MongoDbStorage({
-//     url: process.env.MONGO_URI,
-//   });
-// }
-//
-// const adapter = new FacebookAdapter({
-//
-//   verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
-//   access_token: process.env.FACEBOOK_ACCESS_TOKEN,
-//   app_secret: process.env.FACEBOOK_APP_SECRET,
-// });
-//
-// // emit events based on the type of facebook event being received
-// adapter.use(new FacebookEventTypeMiddleware());
-//
-//
-// const controller = new Botkit({
-//   webhook_uri: '/api/messages',
-//
-//   adapter,
-//
-//   storage,
-// });
-//
-// if (process.env.cms_uri) {
-//   controller.usePlugin(new BotkitCMSHelper({
-//     uri: process.env.cms_uri,
-//     token: process.env.cms_token,
-//   }));
-// }
-//
-// // load traditional developer-created local custom feature modules
-// controller.ready(() => {
-//   // load traditional developer-created local custom feature modules
-//   // controller.loadModules(`${__dirname}/features`);
-//
-//
-//
-//   /* catch-all that uses the CMS to trigger dialogs */
-//   if (controller.plugins.cms) {
-//     controller.on('message,direct_message', async (bot, message) => {
-//       let results = false;
-//       results = await controller.plugins.cms.testTrigger(bot, message);
-//       return true;
-//
-//       if (results !== false) {
-//         // do not continue middleware!
-//         return false;
-//       }
-//     });
-//   }
-//   controller.webserver.get('/', (req, res) => {
-//     res.send(`This app is running Botkit ${controller.version}.`);
-//   });
-// });
-
-
 //  __   __  ___        ___
 // |__) /  \  |  |__/ |  |
 // |__) \__/  |  |  \ |  |
 
-// This is the main file for the FBbotMind bot.
+// This is the main file for the facebot bot.
 
 // Import Botkit's core features
 const { Botkit } = require('botkit');
 const { BotkitCMSHelper } = require('botkit-plugin-cms');
 
+
 // Import a platform-specific adapter for facebook.
 
 const { FacebookAdapter, FacebookEventTypeMiddleware } = require('botbuilder-adapter-facebook');
 
-const { MongoDbStorage } = require('botbuilder-storage-mongodb');
-
+const { MongoDbStorage } = require('botbuilder-storage-mongodb');// const mongoose = require('mongoose');
 
 // Load process.env values from .env file
 require('dotenv').config();
@@ -101,16 +22,10 @@ let storage = null;
 if (process.env.MONGO_URI) {
   storage = mongoStorage = new MongoDbStorage({
     url: process.env.MONGO_URI,
-    database: 'ShopDB',
-    // collection : "AllUsers"
   });
 }
 
-
 const adapter = new FacebookAdapter({
-
-  // REMOVE THIS OPTION AFTER YOU HAVE CONFIGURED YOUR APP!
-  // enable_incomplete: true,
 
   verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
   access_token: process.env.FACEBOOK_ACCESS_TOKEN,
@@ -136,14 +51,19 @@ if (process.env.cms_uri) {
   }));
 }
 
+// load traditional developer-created local custom feature modules
 controller.ready(() => {
-  // controller.loadModules(__dirname + '/features');
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(`${__dirname}/features`);
+
+
 
   /* catch-all that uses the CMS to trigger dialogs */
   if (controller.plugins.cms) {
     controller.on('message,direct_message', async (bot, message) => {
       let results = false;
       results = await controller.plugins.cms.testTrigger(bot, message);
+      return true;
 
       if (results !== false) {
         // do not continue middleware!
@@ -151,12 +71,7 @@ controller.ready(() => {
       }
     });
   }
-});
-
-
-controller.webserver.get('/', (req, res) => {
-  res.send(`This app is running Botkit ${controller.version}.`);
-});
-controller.webserver.get('/konf', (req, res) => {
-  res.send('It\'s fine');
+  controller.webserver.get('/', (req, res) => {
+    res.send(`This app is running Botkit ${controller.version}.`);
+  });
 });
